cd2074b7ff6c0aaf4dbac56f672e62cbcadb1928
Alberto Mijares <alberto.mijares@edorasware.com>
2015-08-02 13:33:38 +0200

    (feat): add support to extract the navigation bar items from the routes

--- src/lib/navigation-bar/index.js
+++ src/lib/navigation-bar/index.js
@@ -1,9 +1,13 @@
 require('./index.css');
 
 var angular = require('angular'),
+    angularRoute = require('angular-route'),
     navigationBarDirective = require('./navigation-bar-directive'),
     navigationBarService = require('./navigation-bar-service');
 
-module.exports = angular.module('showcase-component-commons.navigation-bar', [])
+module.exports = angular.module('showcase-component-commons.navigation-bar',
+    [
+        angularRoute
+    ])
     .directive('navigationBar', navigationBarDirective)
     .provider('navigationBarService', navigationBarService);
--- src/lib/navigation-bar/navigation-bar-service.js
+++ src/lib/navigation-bar/navigation-bar-service.js
@@ -1,7 +1,7 @@
 var assert = require('js-commons').assert;
 
 module.exports = function () {
-    var navigationBarItems;
+    var navigationBarItems = [];
 
     this.setItems = function (items) {
         assert.isArray(items);
@@ -9,11 +9,43 @@ module.exports = function () {
         navigationBarItems = items;
     };
 
-    this.$get = function navigationbarFactory() {
+    this.$get = function ($route) {
+        if (navigationBarItems.length === 0) {
+            navigationBarItems = extractNavigationBarItemsFromRoutes($route.routes);
+        }
+
         return {
             getItems: function () {
                 return navigationBarItems;
             }
         };
     };
+
+    function extractNavigationBarItemsFromRoutes(routes) {
+        return extractRoutes(routes)
+            .filter(hasTitleProperty)
+            .sort(sortByOrderProperty)
+            .map(toNavigationBarItem);
+
+        function extractRoutes(routesObject) {
+            return Object.keys(routesObject).map(function (routeKey) {
+                return routesObject[routeKey]
+            });
+        }
+
+        function hasTitleProperty(route) {
+            return !!route.title;
+        }
+
+        function sortByOrderProperty(route1, route2) {
+            return route1.order > route2.order;
+        }
+
+        function toNavigationBarItem(route) {
+            return {
+                title: route.title,
+                url: route.path || route.originalPath
+            };
+        }
+    }
 };
